789ffeecfe163a9a2c6587d23f9fe9d0dee51300
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-05 08:58:14 +0900

    blogs_controller追記

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,5 +8,6 @@ class BlogsController < ApplicationController
 
   def create
     Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    redirect_to new_blog_path
   end
 end
