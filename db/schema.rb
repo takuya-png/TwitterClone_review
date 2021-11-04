9dd1d3e715d25e511320686a002bf89ced634db2
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-04 14:52:17 +0900

    ロールバック練習

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 2021_11_04_053550) do
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
