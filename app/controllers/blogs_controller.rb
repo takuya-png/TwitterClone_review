91e0f9b2579c5cc4b0acb8ba054cb8060cf3c3a9
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-05 12:36:37 +0900

    ストロングパラメータ記述

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,12 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    Blog.create(params.require(:blog).permit(:title, :content))
     redirect_to new_blog_path
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
