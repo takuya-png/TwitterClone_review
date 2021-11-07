1e328221e1c4d18f022742f0cb72615912d5d7d1
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-07 16:26:34 +0900

    updateアクション追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,6 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
-    raise
   end
 
   def new
@@ -23,6 +22,15 @@ class BlogsController < ApplicationController
 
   def edit
     @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
