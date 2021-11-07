363083dc0b8ec44606c46f4418e2d014c97f9d31
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-07 16:40:28 +0900

    共通処理のメソッド化、viewの共通化

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
+
   def index
     @blogs = Blog.all
   end
@@ -17,15 +19,12 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました！"
     else
@@ -36,5 +35,9 @@ class BlogsController < ApplicationController
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
