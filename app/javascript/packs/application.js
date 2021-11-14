cdff052472d89098b31fb7abae6dd638d0258225
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-14 15:36:09 +0900

    タイトル横にボタンを設置

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
 import '../stylesheets/application';
+import '../stylesheets/bootstrap_overrides.scss';
 
 Rails.start()
 Turbolinks.start()
