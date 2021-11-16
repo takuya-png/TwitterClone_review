e3a30b2b29142072497d6fcc0ab22b046038925f
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-16 20:20:42 +0900

    セッションとクッキー

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: 'tops#index'
   resources :blogs
-  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
