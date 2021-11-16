78b8734b1f1ef21c2504f5ecd44935b6d0d6d80d
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-16 23:51:37 +0900

    ログアウト機能作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
   end
 
@@ -11,5 +13,11 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
